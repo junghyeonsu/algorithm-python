c2e6d57f785cafc0b33d67dd52cda85a5077fdf5
junghyeonsu <jung660317@naver.com>
2021-01-01 21:30:42 +0900

    동빈나 유튜브 파이썬 기초 강좌 15강

---
+++ dongbinNa youtube/15강.py
@@ -0,0 +1,10 @@
+a = "Hello World!"
+
+# b = a.replace("Hello", "Hi") # Hello를 Hi로 바꾸기, 주의할 점은 변수에 할당해줘야 함
+# print(a.count('o')) # o가 나오는 횟수
+# print(a.find("Worl")) # Worl의 위치 인덱스를 나타내준다.
+# b = a.strip("Hello")  # 지우는 것
+# b = a.split(" ") # " "을 기준으로 나누고 배열로 반환
+# b = a.zfill(50) # zero fill 앞에 자리를 매꿔버림
+#  int() # 문자를 숫자로 변환 가능
+
