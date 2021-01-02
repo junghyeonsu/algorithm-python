a8e5c564b05dd15d7155f38dd3fdf6893647dafb
junghyeonsu <jung660317@naver.com>
2021-01-02 21:56:50 +0900

    21강 수강완료

---
+++ dongbinNa youtube/21강.py
@@ -0,0 +1,10 @@
+# input 함수
+print("a, b를 입력해주세요")
+a = input().split(' ')
+# a = 배열로 저장
+x = int(a[0])
+y = int(a[1])
+print("x + y = ", x + y)
+print("x - y = ", x - y)
+print("x * y = ", x * y)
+print("x / y = ", x / y)
