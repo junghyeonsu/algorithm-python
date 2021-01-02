f4952c437a8cebb1d6af2abd82a4a3a882f7b4e7
junghyeonsu <jung660317@naver.com>
2021-01-02 21:43:07 +0900

    16강 수강완료

--- dongbinNa youtube/16강.py
+++ dongbinNa youtube/16강.py
@@ -1,4 +1,15 @@
-a = 10
-b = 25.3
-print(a + b)
 # 실수 + 정수 = 실수
+# 문자열 + 실수(정수) = 오류
+a = 9
+b = 7
+print("a + b : ", a + b)
+print("a - b : ", a - b)
+print("a * b : ", a * b)
+print("a / b : ", a / b) # 소수점까지 계산을 해준다.
+print("a // b : ", a // b) # 몫만 얻고싶을 때
+print("a % b : ", a % b)
+
+# 복소수 계산도 해준다.
+a = (1 + 2j)
+b = (3 + 4j)
+print(a * b)
